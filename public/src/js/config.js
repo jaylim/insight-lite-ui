002d1c9c45af6605c79795d76d72bb4a0ab5d764
Jay <jay@pentajeu.com>
2018-06-13 17:51:35 +0800

    remove litecoin wording

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Litecoin Block '
+      title: 'Unihash Block '
     }).
     // when('/block-index/:blockHeight', {
     //   controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Litecoin Transaction '
+      title: 'Unihash Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Litecoin Blocks solved Today'
+      title: 'Unihash Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Litecoin Blocks solved '
+      title: 'Unihash Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Litecoin Address '
+      title: 'Unihash Address '
     }).
     when('/status', {
       templateUrl: 'views/status.html',
